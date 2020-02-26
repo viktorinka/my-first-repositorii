01a3bcaab7ee8c8cbadf16c4a0c0e66b2fb17909
dzherommo <dzherommo@gmail.com>
2020-02-26 12:31:04 +0300

    Первый коммит

---
+++ task1.6.1-different_css_selectors.py
@@ -0,0 +1,32 @@
+'''
+Вам нужно открыть страницу по ссылке и заполнить форму на этой странице с помощью Selenium. Если всё сделано правильно, то вы увидите окно с проверочным кодом. Это число вам нужно ввести в качестве ответа в этой задаче.
+! Обратите внимание, что время для ввода данных ограничено. Однако благодаря Selenium вы сможете выполнить задачу до того, как время истечёт.
+'''
+
+from selenium import webdriver
+import time 
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_tag_name('input')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name('last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name('form-control.city')
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id('country')
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
